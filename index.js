954496fe74e44072b71ed476e9bd32c7364a61f6
Bobby <enomatebobby@gmail.com>
2023-06-21 23:12:43 +0100

    created a CRUD api and added a .env file

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const {pool} = require('./queries.js')
+const db = require('./queries');
 const express = require('express');
-const bodyParser = require('body-parser')
-const app = express()
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3002;
 
-app.listen(3002, () => console.log('app is runnning'))
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extented: true,
+  })
+)
+
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
 
+app.listen(port, () => console.log(`app is runnning on ${port}`))
 
-const testPostgres = async () => {
-  const result = await pool.query('SELECT $1::text as name', ['db connected'])
-  console.log(result.rows[0].name)
-}
-
-testPostgres();
+// GET: / | displayHome()
+// GET: /users | getUsers()
+// GET: /users/:id | getUserById()
+// POST: /users | createUser()
+// PUT: /users/:id | updateUser()
+// DELETE: /users/:id | deleteUser()
--- queries.js
+++ queries.js
@@ -1,11 +1,83 @@
+require('dotenv').config()
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'tehzz',
+  user: process.env.DB_USERNAME,
   host: 'localhost',
-  database: 'api',
-  password: 's@m123u3l',
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: 5432
 })
+const testPostgres = async () => {
+  const result = await pool.query('SELECT $1::text as name', ['db connected'])
+  console.log(result.rows[0].name)
+}
 
+testPostgres();
 
-module.exports = {pool}
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createUser = (request, response) => {
+  const { name, email } = request.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+    console.log()
+  })
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  pool,
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+}
