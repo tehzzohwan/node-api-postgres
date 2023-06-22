7eea284796db2410288b6538033dad362fd742d6
Bobby <enomatebobby@gmail.com>
2023-06-22 01:46:20 +0100

    added eslint to fix errors in the index.js and queries.js files

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-const db = require('./queries');
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./queries');
+
 const app = express();
 const port = 3002;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extented: true,
-  })
-)
+  }),
+);
 
 app.get('/', (req, res) => {
-  res.json({ info: 'Node.js, Express, and Postgres API' })
-})
-
+  res.json({ info: 'Node.js, Express, and Postgres API' });
+});
 app.get('/users', db.getUsers);
 app.get('/users/:id', db.getUserById);
 app.post('/users', db.createUser);
 app.put('/users/:id', db.updateUser);
 app.delete('/users/:id', db.deleteUser);
 
-
-app.listen(port, () => console.log(`app is runnning on ${port}`))
-
-// GET: / | displayHome()
-// GET: /users | getUsers()
-// GET: /users/:id | getUserById()
-// POST: /users | createUser()
-// PUT: /users/:id | updateUser()
-// DELETE: /users/:id | deleteUser()
+app.listen(port, () => console.log(`app is runnning on ${port}`));
